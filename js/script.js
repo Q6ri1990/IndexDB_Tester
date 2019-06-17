7ffe7d05a1fe1ebcd7530a580028b59d39eb4fb4
Ali Al-Sahli <alial-sahli@Alis-MacBook-Pro.local>
2019-06-17 17:44:56 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-// load service worker
-
-
 
 // based on code from https://codelabs.developers.google.com/codelabs/workbox-indexeddb/index.html
-const dbName = 'dashboardr',
+// idb promised github https://github.com/jakearchibald/idb
+const dbName = 'dashboard',
     tableName = 'events',
     version = 1;
+
+
+    class testData{
+        constructor(id, name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    
+    
+    }
 
 function isIndexDBSupported() {
     if (!('indexedDB' in window)) {
@@ -17,14 +26,32 @@ function isIndexDBSupported() {
     alert("IDB Supported");
     return true;
 }
+/*
+// create the database 
+const db = openDB(dbName, version, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+      // …
+      console.log("upgrade DB");
+    },
+    blocked() {
+      // …
+    },
+    blocking() {
+      // …
+    }
+  });*/
+
+
+
+
 
 // create the database
 function createIndexedDB() {
     if (isIndexDBSupported()) {
         return idb.open(dbName, version, function (upgradeDb) {
-            alert("Creating DB "+dbName);
+            alert("Creating DB " + dbName);
             if (!upgradeDb.objectStoreNames.contains(tableName)) {
-                alert("upgrading table "+tableName);
+                alert("upgrading table " + tableName);
                 const eventsOS = upgradeDb.createObjectStore(tableName, {
                     keyPath: 'id'
                 });
@@ -35,7 +62,7 @@ function createIndexedDB() {
 
 // create the promis object that will be used by the application
 const dbPromise = createIndexedDB();
-
+/*
 
 
 // save the data that we want into the Local IDB
@@ -67,7 +94,7 @@ function loadContentNetworkFirst() {
                 });
         }).catch(err => {
             console.log('Network requests have failed, this is expected if offline');
-            getLocalEventData()
+            getAllData()
                 .then(offlineData => {
                     if (!offlineData.length) {
                         messageNoData();
@@ -86,7 +113,7 @@ function addAndPostEvent() {
     // ...
 }
 
-function getLocalEventData() {
+function getAllData() {
     if (isIndexDBSupported()) {
         return dbPromise.then(db => {
             const tx = db.transaction(tableName, 'readonly');
@@ -95,17 +122,61 @@ function getLocalEventData() {
         });
     }
 }
+*/
+var array = [new testData('1','test'), new testData('2','ali')];
 
-var array = [{
-name:'test'
-},
-{
-    name:'ali'
-}];
+function saveEventDataLocally(records) {
+    if (isIndexDBSupported()) {
+        return dbPromise.then(db => {
+            const tx = db.transaction(tableName, 'readwrite');
+            const store = tx.objectStore(tableName);
+            return Promise.all(records.map(record => store.put(record)))
+                .catch(() => {
+                    tx.abort();
+                    throw Error('Events were not added to the store');
+                });
+        });
+    }
+}
+
+
+function getAllData(){
+ var data = null;   
+if(isIndexDBSupported()){
+    data = dbPromise.then(function(db) {
+        var tx = db.transaction(tableName, 'readonly');
+        var store = tx.objectStore(tableName);
+        console.log('getting data from '+tableName)
+        return store.getAll();
+      });
+    }
+    return data;
+}
 
 saveEventDataLocally(array);
 
-console.log(getLocalEventData());
+function populateTable(){
+    var table = document.getElementById('table');
+    getAllData().then(records =>{
+        records.forEach(element => {
+            let row = table.insertRow();
+            var Newcell1 = row.insertCell(0); 
+            var Newcell2 = row.insertCell(1); 
+            Newcell1.innerHTML = element.id; 
+            Newcell2.innerHTML = element.name;
+        });
+    });
+}
 
+populateTable();
 
+function saveName(){
+    
+    let name = document.getElementById("name").value;
+    let id = Math.random();
 
+    console.log('adding '+name);
+    
+    saveEventDataLocally([new testData(id, name)]);
+    populateTable();
+}
